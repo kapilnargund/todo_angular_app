84514b0ffad568053ae174a63b54f5370ac11603
kapilskyindya <kapil.skyindya@gmail.com>
2022-01-16 14:13:48 +0530

    issues with first repo, second attempt

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { title } from 'process';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos:Todo[];
+
+  constructor() { 
+    this.todos = [
+      {
+        sno:1,
+        title: "This is title1",
+        desc:"this is desc1",
+        active:true
+      },
+      {
+        sno:2,
+        title: "This is title2",
+        desc:"this is desc2",
+        active:true
+      },
+      {
+        sno:3,
+        title: "This is title3",
+        desc:"this is desc3",
+        active:true
+      },
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+  deleteTodo(todo:Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index,1)
+  }
+  addTodo(todo:Todo){
+    console.log(todo);
+    this.todos.push(todo);
+  }
+
+}
